83dab6bd00396976b434f29c13f138d6b627bbec
SinhoMoeme <SinhoMoemeHF47@gmail.com>
2026-02-28 00:38:43 +0800

    alpha 0.0.3

--- src/include/shinsei/minimal/struct.h
+++ src/include/shinsei/minimal/struct.h
@@ -5,3 +5,29 @@
 
 #include<stdlib.h>
 #include<string.h>
+
+typedef void (*shinsei_deallocator_t)(const void *const);
+
+typedef struct{
+	const void* val;
+	shinsei_deallocator_t deallocator;
+}shinsei_object_t;
+
+shinsei_object_t* shinsei_object_t_con(const void *const val,shinsei_deallocator_t deallocator);
+
+
+_SHINSEI_LIB_API void shinsei_object_t_dec(shinsei_object_t *const obj);
+
+_SHINSEI_LIB_API bool shinsei_object_t_isLegal(const shinsei_object_t *const obj);
+
+_SHINSEI_LIB_API bool shinsei_object_t_isNullptr(const shinsei_object_t *const obj);
+
+_SHINSEI_LIB_API const void* shinsei_object_t_value(const shinsei_object_t *const obj);
+
+_SHINSEI_LIB_API shinsei_deallocator_t shinsei_object_t_getDeallocator(const shinsei_object_t *const obj);
+
+_SHINSEI_LIB_API void shinsei_object_t_alloc(shinsei_object_t *const obj,const void *const val);
+
+_SHINSEI_LIB_API bool shinsei_object_t_free(shinsei_object_t *const obj);
+
+_SHINSEI_LIB_API void shinsei_object_t_setDeallocator(shinsei_object_t *const obj,shinsei_deallocator_t deallocator);
---
+++ src/struct/struct.c
@@ -0,0 +1,49 @@
+#include"shinsei/minimal/struct.h"
+
+shinsei_object_t* shinsei_object_t_con(const void *const val,shinsei_deallocator_t deallocator){
+	shinsei_object_t* res=(shinsei_object_t*)malloc(sizeof(shinsei_object_t));
+	if(res==nullptr) return nullptr;
+	res->val=val;
+	res->deallocator=deallocator;
+	return res;
+}
+
+
+void shinsei_object_t_dec(shinsei_object_t *const obj){
+	if(shinsei_object_t_isLegal(obj)) obj->deallocator(obj->val);
+	free(obj);
+	return;
+}
+
+bool shinsei_object_t_isLegal(const shinsei_object_t *const obj){
+	return obj->deallocator!=nullptr;
+}
+
+bool shinsei_object_t_isNullptr(const shinsei_object_t *const obj){
+	return obj->val==nullptr;
+}
+
+const void* shinsei_object_t_value(const shinsei_object_t *const obj){
+	return obj->val;
+}
+
+shinsei_deallocator_t shinsei_object_t_getDeallocator(const shinsei_object_t *const obj){
+	return obj->deallocator;
+}
+
+void shinsei_object_t_alloc(shinsei_object_t *const obj,const void *const val){
+	obj->val=val;
+	return;
+}
+
+bool shinsei_object_t_free(shinsei_object_t *const obj){
+	if(!shinsei_object_t_isLegal(obj)) return 0;
+	obj->deallocator(obj->val);
+	obj->val=nullptr;
+	return 1;
+}
+
+void shinsei_object_t_setDeallocator(shinsei_object_t *const obj,shinsei_deallocator_t deallocator){
+	obj->deallocator=deallocator;
+	return;
+}
